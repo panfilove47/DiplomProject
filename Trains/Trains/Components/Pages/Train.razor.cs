6a19411f8c2de6afc65c151591e95f6d821ed064
panfilove47 <mixailpanf@yandex.ru>
2024-06-11 02:50:05 +0300

    Добавил графики о движении поездов

--- Trains/Trains/Components/Pages/Train.razor.cs
+++ Trains/Trains/Components/Pages/Train.razor.cs
@@ -1,4 +1,6 @@
 ﻿using ChartJs.Blazor.Common;
+using ChartJs.Blazor.Common.Axes;
+using ChartJs.Blazor.Common.Axes.Ticks;
 using ChartJs.Blazor.LineChart;
 using ChartJs.Blazor.PieChart;
 using ChartJs.Blazor.Util;
@@ -21,10 +23,13 @@ namespace Trains.Components.Pages
 
         private IEnumerable<TrainMovementsDto> movements;
 
-        private PieConfig _config;
+        private LineConfig _config;
 
-        protected override void OnInitialized()
+        protected override async void OnInitialized()
         {
+            train = await trainRepository.GetTrainById(Id);
+
+            movements = await trainRepository.GetMovementsById(Id);
             AddDiagramm();
             base.OnInitialized();
         }
@@ -40,34 +45,102 @@ namespace Trains.Components.Pages
 
         public void AddDiagramm()
         {
-            _config = new PieConfig();
+            _config = new LineConfig();
 
-            _config.Options = new PieOptions
+            _config.Options = new LineOptions
             {
                 Responsive = true,
                 Title = new OptionsTitle
                 {
                     Display = true,
-                    Text = "Zalupa"
-                }
+                    Text = "Анализ движения поездов"
+                },
+                //Scales = new Scales
+                //{
+                //    XAxes = new List<CartesianAxis>
+                //{
+                //    new LinearCartesianAxis
+                //    {
+                //        ScaleLabel = new ScaleLabel
+                //        {
+                //            Display = true,
+                //            LabelString = "Поезда"
+                //        },
+                //        Ticks = new LinearCartesianTicks
+                //        {
+                //            AutoSkip = false
+                //        }
+                //    }
+                //},
+                //    YAxes = new List<CartesianAxis>
+                //{
+                //    new LinearCartesianAxis
+                //    {
+                //        ScaleLabel = new ScaleLabel
+                //        {
+                //            Display = true,
+                //            LabelString = "Время (часы)"
+                //        },
+                //        Ticks = new LinearCartesianTicks
+                //        {
+                //            Min = DateTime.Now.Date.ToOADate(), // Минимальное значение (например, начало текущего дня)
+                //            Max = DateTime.Now.Date.AddDays(1).ToOADate()
+                //        }
+                //    }
+                //}
+                //}
             };
 
-            foreach (var party in new[] {"Party1", "Party2", "Party3"})
+            foreach (var item in movements.Select(m => m.To))
             {
-                _config.Data.Labels.Add(party);
+                _config.Data.Labels.Add(item);
+            }
+            //_config.Data.Labels.(movements.Select(m => m.Name));
+
+            var scheduledArrivals = new LineDataset<double>
+            {
+                Label = "Запланированное время прибытия",
+                BackgroundColor = ColorUtil.ColorHexString(0, 0, 255),
+                BorderColor = ColorUtil.ColorHexString(0, 0, 255),
+                Fill = false
+            };
+
+            var actualArrivals = new LineDataset<double>
+            {
+                Label = "Фактическое время прибытия",
+                BackgroundColor = ColorUtil.ColorHexString(153, 0, 0),
+                BorderColor = ColorUtil.ColorHexString(153, 0, 0),
+                Fill = false
+            };
+
+            var scheduledDepartures = new LineDataset<double>
+            {
+                Label = "Запланированное время отправления",
+                BackgroundColor = ColorUtil.ColorHexString(204, 0, 255),
+                BorderColor = ColorUtil.ColorHexString(204, 0, 255),
+                Fill = false
+            };
+
+            var actualDepartures = new LineDataset<double>
+            {
+                Label = "Фактическое время отправления",
+                BackgroundColor = ColorUtil.ColorHexString(0, 255, 0),
+                BorderColor = ColorUtil.ColorHexString(0, 255, 0),
+                Fill = false
+            };
+
+            foreach (var movement in movements)
+            {
+                scheduledArrivals.Add(movement.ScheduleArrivalTime.ToOADate());
+                actualArrivals.Add(movement.RealArrivalTime.ToOADate() * 2);
+                scheduledDepartures.Add(movement.ScheduleDepartureTime.ToOADate());
+                actualDepartures.Add(movement.RealDepartureTime.ToOADate() * 2);
             }
 
-            var dataset = new PieDataset<int>(new[] { 35, 50, 15 })
-            {
-                BackgroundColor = new[]
-                {
-                    ColorUtil.ColorHexString(255, 0, 0),
-                    ColorUtil.ColorHexString(0, 255, 0),
-                    ColorUtil.ColorHexString(0, 0, 255)
-                }
-            };
-
-            _config.Data.Datasets.Add(dataset);
+            _config.Data.Datasets.Add(scheduledArrivals);
+            _config.Data.Datasets.Add(actualArrivals);
+            _config.Data.Datasets.Add(scheduledDepartures);
+            _config.Data.Datasets.Add(actualDepartures);
         }
 
     }
